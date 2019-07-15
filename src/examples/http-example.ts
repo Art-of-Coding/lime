23454b41124c8e923e6f77b19ee59d1a10717526
Michiel van der Velde <michiel@michielvdvelde.nl>
2019-07-16 01:13:39 +0200

    Update http-example.ts

--- src/examples/http-example.ts
+++ src/examples/http-example.ts
@@ -11,38 +11,45 @@ interface HttpContext extends Context {
   json?: any
 }
 
-const app = new Lime<HttpContext>() // entry app
-const app2 = new Lime<HttpContext>() // index (/) app
-const app3 = new Lime<HttpContext>() // about (/about) app
+const app = new Lime<HttpContext>() // entry app (/)
+const app2 = new Lime<HttpContext>() // about app (/about)
+const app3 = new Lime<HttpContext>() // about (/contact) app
 
-app.use(async (ctx, next: Function) => {
-  if (ctx.req.url === '/') {
+app.use(async (ctx, next) => {
+  if (ctx.req.url === '/about') {
     await app2.run(ctx)
   } else {
     await next()
   }
 })
 
-app.use(async (ctx, next: Function) => {
-  if (ctx.req.url === '/about') {
+app.use(async (ctx, next) => {
+  if (ctx.req.url === '/contact') {
     await app3.run(ctx)
   } else {
     await next()
   }
 })
 
-app2.use(async ctx => {
+app.use(async ctx => {
   ctx.body = 'welcome to /'
 })
 
-app3.use(async ctx => {
-  ctx.json = { message: 'Welcome to /about' }
+app2.use(async ctx => {
+  ctx.body = 'welcome to /about'
 })
 
-const server = createServer((req, res) => {
+app3.use(async ctx => {
+  ctx.json = { message: 'Welcome to /contact' }
+})
+
+const server = createServer(async(req, res) => {
   const ctx: HttpContext = { req, res, status: 404 }
 
-  app.run(ctx).then(() => {
+  try {
+    // Run the middleware for the context
+    await app.run(ctx)
+
     let contentType = 'text/plain'
 
     if (ctx.json) {
@@ -64,9 +71,9 @@ const server = createServer((req, res) => {
       res.writeHead(ctx.status)
       res.end()
     }
-  }).catch(err => {
-    console.error(err)
-  })
+  } catch (e) {
+    console.log(e)
+  }
 })
 
 server.listen(3000, () => {
